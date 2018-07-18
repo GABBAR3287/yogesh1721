f863a264742ee303c942aa04a626f3b705d93345
yogesh <ramnathkerurvesh23@gmail.com>
2018-07-18 14:55:22 +0530

    ata tari ja commit baba

---
+++ Employment/src/app.java
@@ -0,0 +1,15 @@
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		staff staff1 = new NonTeachingstaff();
+		staff staff2 = new Teachingstaff();
+		
+		staff1.calculatesalary();
+		staff2.calculatesalary();
+		
+
+	}
+
+}
